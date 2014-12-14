7286bce57a52b066b0b88e17062f075486b71533
Kim Jeker <info@kije.ch>
2014-12-14 16:15:30 +0100

    Updated menu

--- app/menu.php
+++ app/menu.php
@@ -14,5 +14,12 @@ Menu::make('Admin', function (\Lavary\Menu\Builder $menu) {
     $eventsItem = $menu->add('Events', array('route' => 'show.index'));
     $eventsItem->add('Program', array('route' => 'show.index'));
     $eventsItem->add('Archive', array( 'route' => 'show.archive'))->divide(); // todo active only on archive
+    $eventsItem->add('Manage Events', array('route' => 'admin.event.index'));
     $eventsItem->add('Create Event', array('route' => 'admin.event.create'));
+
+    $usersItem = $menu->add('Users', array('route' => 'admin.user.index'));
+    $usersItem->add('Manage Users', array('route' => 'admin.user.index'));
+    $usersItem->add('Create User', array('route' => 'admin.user.create'));
+
+    // todo settings page
 });
